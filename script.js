d13da590b88943316948d5227f6fa6ec97ebd0b7
Fabrice Karenzi <kafabkaren@gmail.com>
2024-12-10 00:52:24 +0100

    Updated car array

--- script.js
+++ script.js
@@ -64,45 +64,138 @@ window.addEventListener('resize', () => {
   }, 250);
 });
 
-
-
-
-
-// array med bilar -----------------------------------------------------------------------------------------
-const cars = [
+// Array med bilar -----------------------------------------------------------------------------------------
+const carInformation = [
   {
-      id: 1,
-      image: 'https://objectstorage.eu-amsterdam-1.oraclecloud.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F1.png',
-      name: 'Tesla Model S',
-      type: 'Elbil',
-      year: 2023,
-      price: '799 000 SEK'
+    "id": 1,
+    "img":"../assets/McLaren.jpg",
+    "brand": "McLaren",
+    "model": "Artura",
+    "year": 2022,
+    "price": 225000
   },
   {
-      id: 2,
-      image: 'https://objectstorage.eu-amsterdam-1.oraclecloud.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F2.png',
-      name: 'Volvo XC90',
-      type: 'SUV',
-      year: 2021,
-      price: '599 000 SEK'
+    "id": 2,
+    "img":"../assets/Riley.jpg",
+    "brand": "Riley",
+    "model": "Riley Nine Roadster",
+    "year": 1939,
+    "price": 50000
+  },
+  {
+    "id": 3,
+    "img":"../assets/Lamborghini.jpg",
+    "brand": "Lamborghini",
+    "model": "Troligtvis",
+    "year": 2020,
+    "price": 300000
+  },
+  {
+    "id": 4,
+    "img":"../assets/Porsche-911.jpg",
+    "brand": "Porsche",
+    "model": "911 GT3",
+    "year": 2022,
+    "price": 200000
+  },
+  {
+    "id": 5,
+    "img":"../assets/Mercedes-Benz.jpg",
+    "brand": "Mercedes-Benz",
+    "model": "AMG GT R",
+    "year": 2021,
+    "price": 162900
+  },
+  {
+    "id": 6,
+    "img":"../assets/Chevrolet.jpg",
+    "brand": "Chevrolet",
+    "model": "Corvette ZR1",
+    "year": 2019,
+    "price": 135000
+  },
+  {
+    "id": 7,
+    "img":"../assets/Rolls-RoyceGhost.jpg",
+    "brand": "Rolls-Royce",
+    "model": "Ghost",
+    "year": 2020,
+    "price": 400000
+  },
+  {
+    "id": 8,
+    "img":"../assets/Porsche-Cayman.jpg",
+    "brand": "Porsche",
+    "model": "718 Cayman",
+    "year": 2020,
+    "price": 100000
+  },
+  {
+    "id": 9,
+    "img":"../assets/Rolls-Royce.jpg",
+    "brand": "Rolls-Royce",
+    "model": "MyModel",
+    "year": 2020,
+    "price": 160000
+  },
+  {
+    "id": 10,
+    "img":"../assets/McLaren-Artura.jpg",
+    "brand": "McLaren",
+    "model": "720S",
+    "year": 2017,
+    "price": 300000
   }
-  // Fler bilar kan läggas till här
 ];
 
-// Hämta ID från URL:en
-const urlParams = new URLSearchParams(window.location.search);
-const carId = parseInt(urlParams.get('id'), 10);
-
-// Hitta aktuell bil
-const car = cars.find(c => c.id === carId);
-
-// Uppdatera sidan med bilens information
-if (car) {
-  document.getElementById('car-image').src = car.image;
-  document.getElementById('car-name').textContent = car.name;
-  document.getElementById('car-type').textContent = `Typ: ${car.type}`;
-  document.getElementById('car-year').textContent = `År: ${car.year}`;
-  document.getElementById('car-price').textContent = `Pris: ${car.price}`;
-} else {
-  document.querySelector('.car-page').innerHTML = '<p>Bilen hittades inte!</p>';
+// Rendera alla bilars cards
+function populateCarCards(car) {
+  return `
+      <div class="card mb-4 shadow-sm">
+        <a href="car-details.html?id=${car.id}" id="${car.id}">
+          <img class="card-img-top" src="${car.img}" alt="${car.brand} ${car.model}">
+        </a>
+        <div class="card-body">
+          <h6 class="card-title">${car.brand} - ${car.model}</h6>
+          <p class="card-text justify-content-center">År: ${car.year}</p>
+          <p class="card-text">Pris: ${car.price} SEK</p>
+        </div>
+      </div>
+  `;
 }
+
+const container = document.getElementById("carCardsContainer");
+carInformation.forEach(car => {
+  container.innerHTML += populateCarCards(car);
+});
+
+    // Function to get query parameter from URL
+    function getQueryParam(name) {
+      const urlParams = new URLSearchParams(window.location.search);
+      return urlParams.get(name);
+    }
+
+    // Fetch the car ID from the URL query string
+    const carId = getQueryParam('id');
+
+    // Find the car with the corresponding ID from the carInformation array
+    const car = carInformation.find(c => c.id == carId);
+
+    // Display car details
+    if (car) {
+      const carDetailsContainer = document.getElementById("carDetailsContainer");
+      carDetailsContainer.innerHTML = `
+        <div class="card">
+          <img class="card-img-top" src="${car.img}" alt="${car.brand} ${car.model}">
+          <div class="card-body">
+            <h5 class="card-title">${car.brand} ${car.model}</h5>
+            <p class="card-text">Year: ${car.year}</p>
+            <p class="card-text">Price: ${car.price} SEK</p>
+            <p class="card-text">Description of the car can go here...</p>
+          </div>
+        </div>
+      `;
+    } else {
+      document.getElementById("carDetailsTitle").textContent = "Car Not Found";
+      document.getElementById("carDetailsContainer").textContent = "The car you're looking for does not exist.";
+    }
